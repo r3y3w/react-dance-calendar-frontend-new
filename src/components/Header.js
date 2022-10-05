303535e377212be52d9c3066f2e960764db21c9e
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-10-04 21:47:20 -0400

    change Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ const Header = () => {
         <ul>
             <li><a href='/'>Home</a></li>
             <li><a href='/learn2dance'>Learn To Dance</a></li>
-            <li><a href='/admin-page'>Admin Page</a></li>
         </ul>
     )
 }
